ff1a6ac5146b26b0a677c0a3714b530441690a04
math-bl <31114034+math-bl@users.noreply.github.com>
2018-03-27 21:13:39 +0200

    Add count app as a second activity Can restart count (need to save counter)

--- app/src/main/java/com/example/mathildapp/MainActivity.java
+++ app/src/main/java/com/example/mathildapp/MainActivity.java
@@ -1,34 +1,23 @@
 package com.example.mathildapp;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private int mCount = 0;
-    private TextView mShowCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MainActivity", "Hello Lemon World");
-        mShowCount = (TextView) findViewById(R.id.show_count);
     }
 
-    public void showToast(View view) {
-        Context context = getApplicationContext();
-        Toast toast = Toast.makeText(context, R.string.toast_message, Toast.LENGTH_LONG);
-        toast.show();
-    }
+    public void launchCountApp(View view) {
+        Intent intent = new Intent(this, countapp.class);
+        startActivity(intent);
 
-    public void countUp(View view) {
-        mCount++;
-        if (mShowCount != null)
-            mShowCount.setText(Integer.toString(mCount));
     }
 }
---
+++ app/src/main/java/com/example/mathildapp/countapp.java
@@ -0,0 +1,39 @@
+package com.example.mathildapp;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class countapp extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_countapp);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Context context = getApplicationContext();
+        Toast toast = Toast.makeText(context, R.string.toast_message, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+
+    public void restart(View view) {
+        mCount = 0;
+        mShowCount.setText(Integer.toString(mCount));
+    }
+
+}
